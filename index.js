8a2149cd018e782055333924b0436fbfcfdf0b68
Afnavarro27 <andresfelipenavarrovalencia@gmail.com>
2022-08-19 18:40:13 -0500

    Bueno

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express')
 
 const app = express()
 //const PORT = process.env.PORT || 9000;
-app.set('views engine','ejs')
 const path = require('path');
+
+
+app.set('view engine','ejs')
+
 app.set('views',path.join(__dirname,'/views'))
 
 
 
 app.get("/",(req, res) => {
-    console.log(path.__dirname);
+    //console.log(path.__dirname);
     res.render('pages/index')
     
 })
